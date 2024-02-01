c5683b8f0a22d943a88960ebeeeddffad856741b
Michael Graves <30810535+mgraves00@users.noreply.github.com>
2024-02-01 14:54:49 -0600

    when called with 2nd arg equal to the description in the peers file only that entry will be added/updated

--- wireguard/wgadd_peers.sh
+++ wireguard/wgadd_peers.sh
@@ -2,7 +2,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2021 Michael Graves
+# Copyright (c) 2024 Michael Graves
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -29,19 +29,30 @@ if [ -z "$1" ]; then
   # no interface name specified
   exit 1
 fi
+if [ ! -z "$2" ]; then
+  FILTER=$2
+fi
 if [ ! -f $WGPEERFILE ]; then
   # if not peer file just exit nicely... no error
   exit 0
 fi
 
-# routing handled by ifstated
-
 # loop thru peers file and construct ifconfig command
-egrep -v "^#" $WGPEERFILE | while read intf name pub psk port addrs; do
+egrep -v "^#" $WGPEERFILE | while read name descr pub psk port addrs; do
+  if [ ! -z "$FILTER" ]; then
+    if [ "$FILTER" != "$descr" ]; then
+      continue
+    fi
+  fi
   _cmd=""
-  [[ "$intf" != "$1" ]] && continue
-  if [ ! -z "$pub" -a ! -z "$psk" ]; then
-    _cmd="$_cmd wgpeer $pub wgpsk $psk"
+  if [ ! -z "$pub" ]; then
+    _cmd="$_cmd wgpeer $pub"
+  fi
+  if [ ! -z "$psk" -a "$psk" != "\"\"" ]; then
+    _cmd="$_cmd wgpsk $psk"
+  fi
+  if [ ! -z "$descr" -a "$descr" != "\"\"" ]; then
+    _cmd="$_cmd wgdescr $descr"
   fi
   if [ ! -z "$addrs" ]; then
     for addr in $addrs; do
@@ -50,15 +61,18 @@ egrep -v "^#" $WGPEERFILE | while read intf name pub psk port addrs; do
   fi
   if [ ! -z "$name" -a ! -z "$port" ]; then
     # if name == network then the port has alerady been set ... so skip
-    if [ "$name" != "network" ]; then
-      # get the IPv4 Address
-      haddr=`host -tA $name | awk '{ print $4 }'`
-      if [ $? -eq 1 ]; then
-        echo "failed to find address for $name. Aborting"
-        exit 1
-      fi
-      _cmd="$_cmd wgendpoint $haddr $port"
-    fi
+    case "$name" in 
+      network)	;;	# skip
+      remote)	;;	# skip
+      *)
+        # get the IPv4 Address
+        haddr=`host -tA $name | awk '{ print $4 }'`
+	[[ $? -eq 1 ]] && continue
+	[[ "$haddr" == "found:" ]] && (echo "failed to find address for $name. Skipping"; continue)
+	[[ "$haddr" == "pointer" ]] && haddr=$name # IP was specified
+        _cmd="$_cmd wgendpoint $haddr $port"
+        ;;
+    esac
   fi
   ifconfig $1 $_cmd
 done
